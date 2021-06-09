dc5b9921dae8df6604f3454f7b6e7c6055a06e26
somerbne2024 <zugang2009329b@126.com>
2021-06-09 23:16:57 +0800

    测试提交

--- app/src/main/java/com/future/bantang/activity/MainActivity.java
+++ app/src/main/java/com/future/bantang/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.future.bantang;
+package com.future.bantang.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.future.bantang.R;
 
 public class MainActivity extends AppCompatActivity {
 
