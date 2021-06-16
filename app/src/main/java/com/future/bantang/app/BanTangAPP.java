932308604fb035ea7e92bbe81d17a69aa13cf728
somerbne2024 <zugang2009329b@126.com>
2021-06-16 14:10:34 +0800

    测试提交

---
+++ app/src/main/java/com/future/bantang/app/BanTangAPP.java
@@ -0,0 +1,9 @@
+package com.future.bantang.app;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 16-1-11.
+ */
+public class BanTangAPP extends Application{
+}
--- app/src/main/java/com/future/bantang/fragment/BaseFragment.java
+++ app/src/main/java/com/future/bantang/fragment/BaseFragment.java
@@ -6,4 +6,5 @@ import android.support.v4.app.Fragment;
  * Created by Administrator on 16-1-11.
  */
 public class BaseFragment extends Fragment {
+
 }
